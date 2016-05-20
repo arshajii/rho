30f7e1dbd6a2cdfe0751b9c533c897395fe33a33
A. R. Shajii <ars@ars.me>
2016-05-20 17:17:35 -0400

    Fix rho.h includes

--- include/rho.h
+++ include/rho.h
@@ -3,21 +3,26 @@
 
 /* general */
 #include "object.h"
+#include "vm.h"
 #include "vmops.h"
 #include "builtins.h"
 
 /* built-in classes */
-#include "codeobject.h"
-#include "floatobject.h"
+#include "null.h"
 #include "intobject.h"
-#include "iter.h"
+#include "floatobject.h"
+#include "strobject.h"
 #include "listobject.h"
-#include "metaclass.h"
+#include "tupleobject.h"
+#include "setobject.h"
+#include "dictobject.h"
+#include "iter.h"
+#include "codeobject.h"
+#include "funcobject.h"
 #include "method.h"
 #include "module.h"
 #include "nativefunc.h"
-#include "strobject.h"
-#include "tupleobject.h"
+#include "metaclass.h"
 
 /* errors/exceptions */
 #include "exc.h"
